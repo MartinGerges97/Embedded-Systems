a1a262923b4e4cfa384a7d9b1e17d8dc7c18257e
Martin Gerges <martingerges97@gmail.com>
2024-03-27 18:07:17 +0200

    updates on assignment 1

--- Assignment/Assignment 1/Assignment1/Assign1_main.c
+++ Assignment/Assignment 1/Assignment1/Assign1_main.c
@@ -7,25 +7,25 @@
 
 #include "fun_prototypes.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
 	/*===================================================================
 	 Q1: Write a program that take two integers from the user and print the results
 	 of this equation: Result = ((num1 + num2) * 3) – 10
 	  ===================================================================*/
-	question_1();
+	//question_1();
 
 	/*===================================================================
 	 Q2: Write a program that print your name and your grade in a new line
 	   ===================================================================*/
 
-	question_2();
+	//question_2();
 
 	/*===================================================================
 	 Q3: Write a program for converting temperature from degrees Celsius to degrees
 	 Fahrenheit, given the formula: F = C x 95 + 32
 	   ===================================================================*/
-
-
+question_21();
 	return 0;
 }
--- Assignment/ReadMe.txt
+++ Assignment/ReadMe.txt
@@ -1,2 +1,4 @@
 Assignments Files
 Embedded Systems Standard Diploma By Eng. Mohamed Tarek
+
+Assignment 1: Solved, Uploaded.
